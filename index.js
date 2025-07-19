ecb9eb74e3298306afcbb84818a4cd881e579026
Fernando Lopez <frlopez88@icloud.com>
2025-07-19 10:51:58 -0600

    test prod ssl

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 import express from 'express'
 const app = express()
-const port = 443
+import dotenv from 'dotenv'
+dotenv.config()
 import { person } from './routes/personRoute.js'
-import {phone} from './routes/phoneRoute.js'
+import { phone } from './routes/phoneRoute.js'
 import cors from 'cors'
+const port = 3000
 
-app.use( express.json() ) // this enable JSON communication 
+const node_env = process.env.NODE_ENV
+
+app.use(express.json()) // this enable JSON communication 
 app.use(cors())
+app.use('/api/', person)
+app.use('/api/', phone)
+
+if (node_env === 'production') {
+
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/test-ssl.novotechn.com/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/test-ssl.novotechn.com/fullchain.pem', 'utf8');
+
+    const credentials = { key: privateKey, cert: certificate };
+
+    // Crear servidor HTTPS en puerto 443
+    https.createServer(credentials, app).listen(443, () => {
+        console.log('Servidor HTTPS escuchando en el puerto 443');
+    });
 
 
-app.use('/api/' , person)
-app.use('/api/' , phone)
+} else {
 
-app.listen(port, ()=>{
+    app.listen(port, () => {
+        console.log(`listening in port ${port}`)
 
-    console.log(`listening in port ${port}`)
+    })
 
-})
+}
+
