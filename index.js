5b936ce4bd16400dfa5f202b5751297e77b2569f
Fernando Lopez <frlopez88@icloud.com>
2025-07-19 10:32:35 -0600

    test port 80

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
-const port = 3000
+const port = 80
 import { person } from './routes/personRoute.js'
 import {phone} from './routes/phoneRoute.js'
 import cors from 'cors'
