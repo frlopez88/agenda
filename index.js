0d45087b0ddb5db2bc2244e3e90437af1ed1a5e6
Fernando Lopez <frlopez88@icloud.com>
2025-07-22 18:17:29 -0600

    add ssl enviroment

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ dotenv.config()
 import { person } from './routes/personRoute.js'
 import { phone } from './routes/phoneRoute.js'
 import cors from 'cors'
+import fs from 'fs'
+import https from 'https'
 
-const port = 80
+const enviroment = process.env.NODE_ENVIROMENT;
+const port = 3000
+
+if (enviroment === 'production') {
+    port = 443
+}
 
 
 app.use(express.json()) // this enable JSON communication 
@@ -14,9 +21,23 @@ app.use(cors())
 app.use('/api/', person)
 app.use('/api/', phone)
 
-app.listen(port, () => {
-    console.log(`listening in port ${port}`)
+if (enviroment === 'production') {
 
-})
+    const options = {
+        key: fs.readFileSync('/etc/letsencrypt/live/ssl-test-2.codex-p4-2025.click/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/ssl-test-2.codex-p4-2025.click/fullchain.pem'),
+    };
+
+    https.createServer(options, app).listen(port, () => {
+        console.log('HTTPS server is running on port 443');
+    });
+
+} else {
+    app.listen(port, () => {
+        console.log(`listening in port ${port}`)
+    })
+}
+
+
 
 
